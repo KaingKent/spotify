c7a5d58a17d55f3a6f9b22a85b7d79d9179fe814
kent <keikain@3c063003ca4d.ant.amazon.com>
2023-11-01 17:02:44 -0700

    Added user input

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
-artist = "gidle"
 
 def get_token():
     auth_string = client_id + ":" + client_secret
@@ -62,15 +61,39 @@ def list(thing):
     for i , t in enumerate(thing):
         print(f"{i+1}. {t['name']}")
 
+def get_user_input(token):
+    user_input = input("What would you like to do?\n1. Search Artist \n2. Search Album\nQuit\nChoosing: ").lower()
+    match user_input:
+        case "1":
+            get_artist(input("What artist would you like to seach?: "), token)
+        case "quit":
+            return False
+        case _:
+            print("unlucky")
 
-token = get_token()
-result = search_artist(token, artist)
-artist_name = result["name"]
-artist_id = result["id"]
-songs = get_songs_by_artist(token, artist_id)
-albums = get_albums_by_artist(token, artist_id)
+def get_artist(user_artist, token):
+    result = search_artist(token, user_artist)
+    artist_name = result["name"]
+    artist_id = result["id"]
 
-print(f"Top 10 songs by {artist_name}: \n")
-list(songs)
-print(f"=========\n{artist_name} Albums: \n")
-list(albums)
+    user_input = int(input("Choose one of the following:\n1. Top songs \n2. Albums\nChoosing: "))
+    
+    if user_input == 1:
+        songs = get_songs_by_artist(token, artist_id)
+        print(f"Top 10 songs by {artist_name}: \n")
+        list(songs)
+    elif user_input == 2:
+        albums = get_albums_by_artist(token, artist_id)
+        print(f"=========\n{artist_name} Albums: \n")
+        list(albums)
+
+def main():
+    token = get_token()
+    
+    while True:
+        t = get_user_input(token)
+        if t == False:
+            return
+
+
+main()
